18a9d2fdc207ede960d8544176132cd18c81d65c
Teo <teoinke@hotmail.com>
2015-05-15 12:46:37 -0500

    Second commit

--- app/src/main/java/com/teoinke/shakeanimation/MainActivity.java
+++ app/src/main/java/com/teoinke/shakeanimation/MainActivity.java
@@ -17,39 +17,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
     }
 
     public void shake(View v) {
-
-        Animation slideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
-//        Animation slideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+        // Create shake effect from xml resource
+        Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+        // View element to be shaken
         TextView s = (TextView) findViewById(R.id.testView);
-        s.startAnimation(slideDown);
+        // Perform animation
+        s.startAnimation(shake);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
